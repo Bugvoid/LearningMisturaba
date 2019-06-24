736aa7ebd74cf7cd85cb045bc05b84a68d9a0e69
Luan Alves <luan.alves@comunike.me>
2019-06-24 13:47:59 -0300

    feat: Code ListSubmit.js, SubmitNewBoard.js and SubmitNewCard.js

---
+++ Trello_Clone/trello-clone/src/Actions/SubmitNewCard.js
@@ -0,0 +1,10 @@
+import { SUBMIT_NEW_CARD } from "./ActionTypes";
+
+export default function submitNewCard(card, cardId, listId) {
+  return dispatch => {
+    dispatch({
+      type: SUBMIT_NEW_CARD,
+      payload: { cardName: card, listId, cardId }
+    });
+  };
+}
