19fcc6bac107812294dff550d5ce7c99ce1a2ef9
Luan Alves <luan.alves@comunike.me>
2019-06-27 11:12:31 -0300

    fix: index.js

--- Trello_Clone/trello-clone/src/index.js
+++ Trello_Clone/trello-clone/src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import App from "./components/App";
-import Store from "./Store.js";
-import "./index.css";
-import registerServiceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import App from './components/App';
+import Store from './Store.js';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={Store}>
